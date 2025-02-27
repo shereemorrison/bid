990ea143d065836007fa5ad5c325f806e3df7053
shereemorrison <sheree.morrison@outlook.com>
2025-02-27 14:28:09 +1100

    Connected to Firebase and added login logic

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -1,5 +1,4 @@
-import 'package:bid/components/my_button.dart';
-import 'package:bid/components/my_drawer.dart';
+
 import 'package:flutter/material.dart';
 
 class IntroPage extends StatelessWidget {
@@ -8,58 +7,52 @@ class IntroPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Intro"),
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-      ),
-
-      backgroundColor: Theme.of(context).colorScheme.surface,
-      body: SafeArea(child: Padding(
-        padding: const EdgeInsets.all(125),
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              //logo
-              Opacity(
-                opacity: 0.5,
-                child: Image.asset(
-                  'assets/images/Logo.png',
-                  width: 80,
-                  height: 80,
+        backgroundColor: Theme.of(context).colorScheme.surface,
+        body: SafeArea(child: Padding(
+          padding: const EdgeInsets.all(125),
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                //logo
+                Opacity(
+                  opacity: 0.5,
+                  child: Image.asset(
+                    'assets/images/Logo.png',
+                    width: 80,
+                    height: 80,
+                  ),
                 ),
-              ),
-
-              const SizedBox(
-                height: 10,
-              ),
-
-              //Title
-              Text(
-                "Believe in Dreams",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 15,
-                  color: Colors.grey[800],
-
+      
+                const SizedBox(
+                  height: 10,
                 ),
-              ),
-              //Subtitle?
-              Text(
-                "Stefan Časić",
-                style: TextStyle(
-                  fontSize: 12,
-                  color: Colors.grey[800],
-
+      
+                //Title
+                Text(
+                  "Believe in Dreams",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 15,
+                    color: Colors.grey[800],
+      
+                  ),
                 ),
-              ),
-              const SizedBox(height: 10),
-
-            ]
+                //Subtitle?
+                Text(
+                  "Stefan Časić",
+                  style: TextStyle(
+                    fontSize: 12,
+                    color: Colors.grey[800],
+      
+                  ),
+                ),
+                const SizedBox(height: 10),
+      
+              ]
+          ),
         ),
-      ),
-    )
-    );
+      )
+      );
   }
 }
 

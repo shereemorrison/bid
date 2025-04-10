abafe56d38652c5da41462fa7ccb56de30880916
Sheree Morrison <shereemorrison@outlook.com>
2025-04-10 21:42:47 +1000

    Refactored account page

---
+++ lib/config/api_keys.dart
@@ -0,0 +1,6 @@
+class ApiKeys {
+  ApiKeys._();
+
+  // Mapbox API key
+  static const String mapboxApiKey = 'pk.eyJ1Ijoic2hlcmVlYmlkIiwiYSI6ImNtOWIyaHQ3MDBjd3gya29iYm8yZmp6ZHUifQ.LQu7ETr7jjHmva4PGWTalg';
+}
